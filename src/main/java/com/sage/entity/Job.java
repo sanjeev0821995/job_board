8dade8628d3a85769c14236d8dfc4d984dd8cf4c
sanjeev0821995 <sanjeev0821995@gmail.com>
2024-07-23 21:26:24 +0530

    Entities

---
+++ src/main/java/com/sage/entity/Job.java
@@ -0,0 +1,66 @@
+package com.sage.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Job extends BaseEntity implements Serializable {
+
+
+    private static final long serialVersionUID = 46835149772133975L;
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name="Eligibilty")
+    private String eligibility;
+
+    @Column(name="Location")
+    private String location;
+
+
+    @Column(name="Description")
+    private String description;
+
+    @Column(name="Designation")
+    private String designation;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private Category category;
+
+    @Column(name = "Skills_Required")
+    private String skills;
+
+    @Column(name = "Salary_Type")
+    private String salaryType;
+
+    @Column(name = "Salary")
+    private String salary;
+
+    @Column(name = "Experience_Required")
+    private String experience;
+
+    @Column(name = "Sort_Order")
+    private int sortOrder;
+
+    @Column(name = "Status")
+    private boolean status;
+
+//    @JsonIgnore
+//    @OneToMany(mappedBy = "job", fetch = FetchType.EAGER,cascade = CascadeType.ALL,orphanRemoval = true)
+//    private Set<JobApplication> jobApplications=new HashSet<>();
+}
---
+++ src/main/java/com/sage/entity/JobApplication.java
@@ -0,0 +1,56 @@
+package com.sage.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serializable;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class JobApplication extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 46835149772133977L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id",nullable = false)
+    private Job job;
+
+    @Column(name = "First_Name")
+    private String firstName;
+
+    @Column(name = "Last_Name")
+    private String lastName;
+
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "Educational_Qualification")
+    private String qualification;
+
+    @Column(name = "Job_Experience")
+    private String experience;
+
+    @Column(name = "Job_Position")
+    private String position;
+
+    @Column(name="Previous_Company")
+    private String company;
+
+    @Lob
+    @Column(name = "Resume")
+    private byte[] resume;
+
+    @Column(name = "FileName")
+    private String fileName;
+}
